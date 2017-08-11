387cd32dd443b928cc04a9dac8e5e0e10c066395
richbl <richbl@gmail.com>
2017-08-11 12:48:58 -0700

    Add directory management functions, replace bash script with go

---
+++ compile_from_source.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+	"go-distributed-motion-s3/dms3libs"
+	"path/filepath"
+)
+
+func main() {
+
+	const (
+		execDir    = "/usr/local/bin"             // set to anywhere seen by $PATH
+		confDir    = "/etc/distributed-motion-s3" // default location to store config files (*.toml)
+		releaseDir = "dms3_release"               // release folder containing all platform builds
+		linuxArm7  = "linux_arm7"                 // Linux Arm7 platform
+		linuxAMD64 = "linux_amd64"                // Linux AMD64 platform
+	)
+
+	// build platform-specific components into release folder
+	//
+	dms3libs.RmDir(releaseDir)
+	dms3libs.MkDir(filepath.Join(releaseDir, linuxArm7))
+	dms3libs.MkDir(filepath.Join(releaseDir, linuxAMD64))
+	dms3libs.MkDir(filepath.Join(releaseDir, "dms3client"))
+	dms3libs.MkDir(filepath.Join(releaseDir, "dms3server/media"))
+	dms3libs.MkDir(filepath.Join(releaseDir, "dms3libs"))
+	dms3libs.MkDir(filepath.Join(releaseDir, "dms3mail"))
+
+	fmt.Print("building dms3 components for Linux/ARM7 platform...")
+	_, err := dms3libs.RunCommand("env GOOS=linux GOARCH=arm GOARM=7 go build -o " + filepath.Join(releaseDir, linuxArm7) + "/go_dms3client go_dms3client.go")
+	dms3libs.CheckErr(err)
+	_, err = dms3libs.RunCommand("env GOOS=linux GOARCH=arm GOARM=7 go build -o " + filepath.Join(releaseDir, linuxArm7) + "/go_dms3server go_dms3server.go")
+	dms3libs.CheckErr(err)
+	_, err = dms3libs.RunCommand("env GOOS=linux GOARCH=arm GOARM=7 go build -o " + filepath.Join(releaseDir, linuxArm7) + "/go_dms3mail go_dms3mail.go")
+	dms3libs.CheckErr(err)
+	fmt.Println("success")
+
+	fmt.Print("building dms3 components for Linux/AMD64 platform...")
+	_, err = dms3libs.RunCommand("env GOOS=linux GOARCH=amd64 go build -o " + filepath.Join(releaseDir, linuxAMD64) + "/go_dms3client go_dms3client.go")
+	dms3libs.CheckErr(err)
+	_, err = dms3libs.RunCommand("env GOOS=linux GOARCH=amd64 go build -o " + filepath.Join(releaseDir, linuxAMD64) + "/go_dms3server go_dms3server.go")
+	dms3libs.CheckErr(err)
+	_, err = dms3libs.RunCommand("env GOOS=linux GOARCH=amd64 go build -o " + filepath.Join(releaseDir, linuxAMD64) + "/go_dms3mail go_dms3mail.go")
+	dms3libs.CheckErr(err)
+	fmt.Println("success")
+
+	// copy service daemons into release folder
+	//
+	fmt.Print("copying service daemons into " + releaseDir + " folder...")
+	dms3libs.CopyFile("dms3client/daemons/systemd/dms3client.service", filepath.Join(releaseDir, "dms3client/dms3client.service"))
+	dms3libs.CopyFile("dms3server/daemons/systemd/dms3server.service", filepath.Join(releaseDir, "dms3server/dms3server.service"))
+	fmt.Println("success")
+
+	// copy dms3server media files into release folder
+	//
+	fmt.Print("copying dms3server media files (WAV) into " + releaseDir + " folder...")
+	dms3libs.CopyFile("dms3server/media/motion_start.wav", filepath.Join(releaseDir, "dms3server/media/motion_start.wav"))
+	dms3libs.CopyFile("dms3server/media/motion_stop.wav", filepath.Join(releaseDir, "dms3server/media/motion_stop.wav"))
+	fmt.Println("success")
+
+	// copy TOML files into release folder
+	//
+	fmt.Print("copying dms3 component config files (TOML) into " + releaseDir + " folder...")
+	dms3libs.CopyFile("dms3client.toml", filepath.Join(releaseDir, "dms3client/dms3client.toml"))
+	dms3libs.CopyFile("dms3server.toml", filepath.Join(releaseDir, "dms3server/dms3server.toml"))
+	dms3libs.CopyFile("dms3libs.toml", filepath.Join(releaseDir, "dms3libs/dms3libs.toml"))
+	dms3libs.CopyFile("dms3mail.toml", filepath.Join(releaseDir, "dms3mail/dms3mail.toml"))
+	fmt.Println("success")
+
+	// copy release folder into /etc/distributed-motion-s3
+	//
+	fmt.Print("copying " + releaseDir + " folder into " + confDir + " folder...")
+	dms3libs.RmDir(confDir)
+	dms3libs.CopyDir(releaseDir, confDir)
+	dms3libs.RmDir(filepath.Join(confDir, linuxArm7))
+	dms3libs.RmDir(filepath.Join(confDir, linuxAMD64))
+	fmt.Println("success")
+
+	// copy dms3 components into /usr/local/bin
+	//
+	fmt.Print("copying Linux/AMD64 dms3 components into " + execDir + " folder (root permissions expected)...")
+	dms3libs.CopyDir(filepath.Join(releaseDir, linuxAMD64), execDir)
+	fmt.Println("success")
+
+	// fmt.Println("copying Linux/Arm7 dms3 components into " + execDir + " folder (root permissions expected)...")
+	// dms3libs.CopyDir(filepath.Join(releaseDir, linuxArm7), execDir)
+	// fmt.Println("success")
+}
--- dms3libs/lib_util.go
+++ dms3libs/lib_util.go
@@ -1,10 +1,13 @@
 package dms3libs
 
 import (
+	"fmt"
+	"io"
 	"log"
 	"os"
 	"os/exec"
 	"path"
+	"path/filepath"
 	"runtime"
 	"strconv"
 	"strings"
@@ -181,4 +184,100 @@ func Format24H(time string) string {
 
 func rightPadToLen(s string, padStr string, pLen int) string {
 	return s + strings.Repeat(padStr, pLen-len(s))
+}
+
+// CheckErr does simple error management (no logging dependencies)
+func CheckErr(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+}
+
+// MkDir creates a new folder with permissions passed in
+func MkDir(newPath string) {
+
+	error := os.MkdirAll(newPath, os.ModePerm)
+	CheckErr(error)
+
+}
+
+// RmDir creates a new folder with permissions passed in
+func RmDir(dir string) {
+
+	if IsFile(dir) {
+		err := os.RemoveAll(dir)
+		CheckErr(err)
+	}
+
+}
+
+// walkDir generates a map of directories (0) and files (1)
+func walkDir(dirname string) map[string]int {
+
+	fileList := map[string]int{}
+
+	error := filepath.Walk(dirname, func(path string, f os.FileInfo, err error) error {
+
+		// exclude root directory
+		if f.IsDir() && f.Name() == dirname {
+			return nil
+		}
+
+		if f.IsDir() {
+			fileList[path] = 0 // directory
+		} else {
+			fileList[path] = 1 // file
+		}
+
+		return nil
+	})
+	CheckErr(error)
+
+	return fileList
+
+}
+
+// CopyFile copies a file from src to dest
+func CopyFile(src string, dest string) {
+
+	srcFile, err := os.Open(src)
+	CheckErr(err)
+	defer srcFile.Close()
+
+	destFile, err := os.Create(dest)
+	CheckErr(err)
+	defer destFile.Close()
+
+	_, err = io.Copy(destFile, srcFile)
+	CheckErr(err)
+
+	err = destFile.Sync()
+	CheckErr(err)
+
+}
+
+// CopyDir copies a directory from srcDir to destDir
+func CopyDir(srcDir string, destDir string) {
+
+	dirTree := walkDir(srcDir)
+
+	// create directory tree...
+	for dirName, dirType := range dirTree {
+
+		if dirType == 0 {
+			MkDir(destDir + "/" + strings.TrimLeft(dirName, srcDir))
+		}
+
+	}
+
+	// ...then copy files into directory tree
+	for dirName, dirType := range dirTree {
+
+		if dirType == 1 {
+			CopyFile(dirName, destDir+"/"+strings.TrimLeft(dirName, srcDir))
+		}
+
+	}
+
 }
